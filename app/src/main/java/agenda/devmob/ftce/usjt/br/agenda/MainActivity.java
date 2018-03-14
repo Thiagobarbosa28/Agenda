a316a0a87f88df3d154994580fe582200cd0a897
arqdsis <arqdsis@mocli10215.animaedu.intranet>
2018-03-14 20:10:21 -0300

    Initial commit

---
+++ app/src/main/java/agenda/devmob/ftce/usjt/br/agenda/MainActivity.java
@@ -0,0 +1,27 @@
+package agenda.devmob.ftce.usjt.br.agenda;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+    public static final String Fila = "br.uajt.deswebmob.agenda.fila";
+    private EditText txtnome;
+    //private Intent ListarContatosActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtnome = (EditText)findViewById(R.id.buscar_fila);
+    }
+    public void buscarContatos(View view) {
+        String fila = txtnome.getText().toString();
+        Intent intent = new Intent(this,ListarContatosActivity.class);
+        intent.putExtra(Fila, fila);
+        startActivity(intent);
+    }
+}
